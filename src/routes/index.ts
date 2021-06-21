f7c1cc28d5994a5a477de1cc1a6fb760137d0018
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-06-21 18:41:17 -0300

    style: add style guide

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,13 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-const routes = Router()
+const routes = Router();
 
 routes.get('/test', (request, response) => {
-  return response.send('Hello NLW')
-})
+  return response.send('Hello NLW');
+});
 
 routes.post('/test-post', (request, response) => {
-  return response.send('[POST] NLW Valotiza')
-})
+  return response.send('[POST] NLW Valotiza');
+});
 
-export { routes }
+export { routes };
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
-import express from 'express'
-import cors from 'cors'
-import { routes } from './routes'
+import cors from 'cors';
+import express from 'express';
 
-const app = express()
+import { routes } from './routes';
 
-app.use(cors())
-app.use(express.json())
-app.use(routes)
+const app = express();
 
-app.listen(3333, () => console.log('Server started on port', 3333))
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => console.log('Server started on port', 3333));
