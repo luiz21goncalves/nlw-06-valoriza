1e39c36fc967b8302519c46a705c21dd73c81b51
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-06-25 21:00:46 -0300

    chore: update routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,6 +6,10 @@ import { authenticateUser } from '../useCases/authenticateUser';
 import { createCompliment } from '../useCases/createCompliment';
 import { createTag } from '../useCases/createTag';
 import { createUser } from '../useCases/createUser';
+import { listAllTags } from '../useCases/listAllTags';
+import { listAllUsers } from '../useCases/listAllUsers';
+import { listUserReceiveCompliments } from '../useCases/listUserReceiveCompliments';
+import { listUserSenderCompliments } from '../useCases/listUserSenderCompliments';
 
 const routes = Router();
 
@@ -22,12 +26,34 @@ routes.post('/users', (request, response) => {
 
 routes.use(ensureAuthenticated);
 
+routes.get('/users', (request, response) => {
+  return listAllUsers().listAllUsersController.handle(request, response);
+});
+
 routes.post('/tags', ensureAdmin, (request, response) => {
   return createTag().createTagController.handle(request, response);
 });
 
+routes.get('/tags', (request, response) => {
+  return listAllTags().listAllTagsController.handle(request, response);
+});
+
 routes.post('/compliments', (request, response) => {
   return createCompliment().createComplimentController.handle(
+    request,
+    response
+  );
+});
+
+routes.get('/users/compliments/send', (request, response) => {
+  return listUserSenderCompliments().listUserSenderComplimentsController.handle(
+    request,
+    response
+  );
+});
+
+routes.get('/users/compliments/receive', (request, response) => {
+  return listUserReceiveCompliments().listUserReceiveComplimentsController.handle(
     request,
     response
   );
